0edcf538fdfdcf165a8be3faa9c3396bd90a91c4
JarkaP <jarka.p@gmail.com>
2019-11-06 15:02:03 +0100

    handle img export to ascii

--- renderer/scripts/app.js
+++ renderer/scripts/app.js
@@ -3,6 +3,8 @@
 const videoPlayer = document.getElementById('video')
 const printButton = document.getElementById('print')
 const asciiContainer = document.getElementById('ascii-container-video')
+const canvas = document.getElementById('video-output-img')
+const context = canvas.getContext('2d')
 
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
@@ -16,4 +18,22 @@ new Jscii({
     el: videoPlayer,
 })
 
-printButton.addEventListener('click', () => {})
+printButton.addEventListener('click', () => {
+    context.drawImage(videoPlayer, 0, 0, 640, 480)
+
+    let image = new Image()
+    image.src = canvas.toDataURL('image/png')
+    image.id = 'ascii-input-img'
+
+    document.body.appendChild(image)
+
+    new Jscii({
+        width: 100,
+        el: document.getElementById('ascii-input-img'),
+        fn: function(str) {
+            document.getElementById('ascii-output-img').innerHTML = str
+        },
+    })
+
+    document.body.removeChild(image)
+})
